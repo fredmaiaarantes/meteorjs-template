5813f1536c8dc606bad2bbc4eb8e563800444863
Frederico Maia Arantes <fredmaiaarantes@gmail.com>
2022-11-09 16:53:33 -0300

    Improve Auth page when user is logged in

--- ui/pages/AuthPage.jsx
+++ ui/pages/AuthPage.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import { Passwordless } from 'meteor/quave:accounts-passwordless-react';
 import { RoutePaths } from '../common/Routes';
+import { useTracker } from 'meteor/react-meteor-data';
 
 // classnames tailwind passwordless:
 // appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-sm font-medium text-gray-700 mt-0 flex justify-end text-indigo-600 hover:text-indigo-500 cursor-pointer justify-center px-4 border-transparent text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 bg-red-50 bg-green-50 p-4 text-red-800 text-green-800 ml-auto pl-3 -mx-1.5 -my-1.5 inline-flex p-1.5 text-red-500 text-green-500 hover:bg-red-100 bg-green-100 focus:ring-offset-red-50 ring-offset-green-50 focus:ring-red-600 ring-green-600 sr-only mt-8 sm:mx-auto sm:w-full sm:max-w-md bg-white py-8 shadow sm:rounded-lg sm:px-10 space-y-6 mt-1
 
 export const AuthPage = () => {
-  const loggedUser = Meteor.user();
+  const userId = useTracker(() => Meteor.userId());
   const history = useHistory();
 
   const onEnterToken = () => {
     history.push(RoutePaths.HOME);
   };
 
-  if (loggedUser) {
+  if (userId) {
     return (
-      <div className="bg-gradient-to-r from-indigo-900 to-indigo-700">
+      <div className="bg-gradient-to-r from-indigo-900 to-indigo-700 pb-10 pt-10">
         <div className="flex flex-col items-center">
-          <h3 className="px-3 py-2 text-base font-medium">
-            You are already authenticated.
+          <h3 className="px-3 py-2 text-xl text-white font-medium">
+            You are already logged in.
           </h3>
-          <button onClick={() => history.push(RoutePaths.HOME)} type="button">
+          <Link
+            to={RoutePaths.HOME}
+            className="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50 sm:w-auto"
+          >
             Go Home
-          </button>
+          </Link>
         </div>
       </div>
     );
@@ -33,12 +37,12 @@ export const AuthPage = () => {
     <div className="bg-gradient-to-r from-indigo-900 to-indigo-700">
       <div className="flex flex-grow flex-col items-center">
         <Passwordless onEnterToken={onEnterToken} />
-        <a
-          onClick={() => history.push(RoutePaths.HOME)}
+        <Link
+          to={RoutePaths.HOME}
           className="mt-5 mb-10 cursor-pointer text-base font-medium text-white hover:text-gray-300"
         >
           Back to Home
-        </a>
+        </Link>
       </div>
     </div>
   );
